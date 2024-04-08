472fc1cc1c57c9512b4fb1e5578c8195068fe8c4
poojaudupa25 <157234308+poojaudupa25@users.noreply.github.com>
2024-04-08 22:48:00 +0530

    Update JK_interface.sv

--- JK_interface.sv
+++ JK_interface.sv
@@ -5,14 +5,3 @@ modport RTL(input clk, J, K, reset, output q);
 modport TB(input q, output J, K, reset);
 endinterface
 
-//Test
-module Test(JK_inter.TB T1);
-initial
-begin
-T1.reset=0; T1.J=1; T1.K=0;
-#10 T1.reset=1;
-#10 T1.J=0; T1.K=1;
-#10 T1.J=1; T1.K=1;
-#100 $finish;
-end 
-endmodule
